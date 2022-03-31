f60c71d0317f9e8d5be39d0c4de0e5fc54b79475
puskapeti <p.puska@lasram.hu>
2022-03-31 19:56:38 +0200

    moved src into mediscara_ws

---
+++ mediscara_ws/src/mediscara/mediscara/scripts/hmi.py
@@ -0,0 +1,441 @@
+import enum
+import sys
+from typing import List, Type
+
+from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot, QThread, QMutex, QUrl, Qt
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QLabel, QListWidget, QListWidgetItem, QWidget
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+import rclpy
+from mediscara.scripts.ros_node import QTROSNode
+from mediscara.config import IPList
+from mediscara.scripts.widgets.layout.gui_ui import Ui_GUIWindow
+from mediscara.scripts.widgets.layout.error_list_item_ui import Ui_ErrorListItem
+from mediscara.scripts.widgets.layout.node_list_item_ui import Ui_NodeListItem
+from mediscara.scripts.logger import Logger
+
+from mediscara.scripts.widgets.layout.statusbar_ui import Ui_StatusBar
+
+
+class LoginStatus(enum.Enum):
+    """Class for storing login statuses
+
+    Manager - INFO GRAFANA
+    Admin - ALL
+    User - CONTROL INFO
+    """
+    LOGGED_OUT = None
+    USER = "user"
+    ADMIN = "admin"
+    MANAGER = "manager"
+
+    __USER_PASS = "user"
+    __ADMIN_PASS = "admin"
+    __MANAGER_PASS = "manager"
+
+    @classmethod
+    def get_pass(cls, lvl):
+        """Returns the selected user's password"""
+        if lvl == cls.LOGGED_OUT:
+            return None
+        elif lvl == cls.USER:
+            return cls.__USER_PASS.value
+        elif lvl == cls.ADMIN:
+            return cls.__ADMIN_PASS.value
+        elif lvl == cls.MANAGER:
+            return cls.__MANAGER_PASS.value
+
+
+# noinspection PyUnresolvedReferences
+class HMIApp(QMainWindow, Ui_GUIWindow):
+    """Base class for the HMI interfaces of the robotic cells"""
+
+    __TAB_INFO = 0
+    __TAB_CONTROL = 1
+    __TAB_NODES = 2
+    __TAB_GRAFANA = 3
+    __TAB_ERROR = 4
+    __TAB_LOGIN = 5
+
+    __NO_LOGIN = "--nologin"
+    __NO_LOCK = "--nolock"
+
+    # region INNER CLASSES *********************************************************************************************
+
+    class ErrorListItem(Ui_ErrorListItem, QWidget):
+        def __init__(self, node_name: str, error_msg: str, error_code: int):
+            super(HMIApp.ErrorListItem, self).__init__()
+            self.setupUi(self)
+            self.label_node_name.setText(node_name)
+            self.label_error_message.setText(error_msg)
+            self.label_error_code.setText(str(error_code))
+
+    class NodeListItem(Ui_NodeListItem, QWidget):
+
+        __STYLESHEET = "padding: 12px; border: 1px solid black; border-radius: 7px; "
+
+        def __init__(self, node_name: str, missing: bool):
+            super(HMIApp.NodeListItem, self).__init__()
+            self.setupUi(self)
+
+            self.label_node_name.setText(node_name)
+            self.missing = missing
+            if missing:
+                self.label_node_name.setStyleSheet(self.__STYLESHEET + "background-color: yellow")
+            else:
+                self.label_node_name.setStyleSheet(self.__STYLESHEET + "background-color: green")
+
+        def __repr__(self):
+            return f"{self.__class__.__name__}({self.label_node_name.text()}, {self.missing})"
+
+    class StatusbarWidget(Ui_StatusBar, QWidget):
+        def __init__(self):
+            super(HMIApp.StatusbarWidget, self).__init__()
+            self.setupUi(self)
+
+            self.label_login.setText("Not logged in")
+            self.set_error_count(0)
+
+        def set_error_count(self, err_count: int):
+            if err_count == 0:
+                self.label_error.setStyleSheet("background-color: white")
+                self.label_error.setText("No errors")
+            else:
+                self.label_error.setStyleSheet("background-color: red;")
+                if err_count == 1:
+                    self.label_error.setText("1 error")
+
+                else:
+                    self.label_error.setText(f"{err_count} errors")
+
+    # endregion
+
+    def __init__(self, name: str, depends_list: List[str], node_class: Type[QTROSNode]):
+        """Constructor method
+
+        :param name: the name of the ROS node in the app
+        :param depends_list: a list of ROS node names on which this ROS node depends
+        :param node_class: Class object derived from the QTROSNode base class, which gets initialized and
+        communicates with the user interface
+        """
+        super(HMIApp, self).__init__()
+        self.setupUi(self)
+
+        self.logger = Logger(parent=None, tag="HMI", level=Logger.DEBUG)
+
+        self.__user_level = None
+
+        """ Create ROS thread """
+        self.ros_thread = QThread()  # create thread
+
+        self.ros_worker = ROSWorker(node_class=node_class,
+                                    node_name=name,
+                                    depends_list=depends_list
+                                    )
+
+        self.ros_worker.add_to_thread(self.ros_thread)
+
+        # connect the signals and slots
+        self.ros_worker.signals.started.connect(self.ros_node_online_callback)
+        self.ros_worker.signals.nodes_loaded.connect(self.nodes_loaded_callback)
+        self.ros_worker.signals.info_loaded.connect(self.info_loaded_callback)
+        self.ros_worker.signals.new_error.connect(self.ros_error_callback)
+
+        self.ros_thread.start()  # start the thread
+
+        """ Create callbacks """
+        self.tabWidget.currentChanged.connect(self.tab_changed_callback)
+
+        """ Tabs initialization """
+        self.tabWidget.setCurrentIndex(self.__TAB_LOGIN)  # login screen
+
+        # login tab
+        self.button_login.clicked.connect(self.login_callback)
+        self.button_logout.clicked.connect(self.logout_callback)
+
+        # nodes tab
+        self.button_refresh.clicked.connect(self.load_nodes)
+
+        # grafana tab
+        self.web_widget = QWebEngineView(self.tab_grafana)
+        self.web_widget.load(QUrl(IPList.Grafana.value))
+        self.grafana_layout.addWidget(self.web_widget)
+
+        # error tab
+        self.button_clear_errors.clicked.connect(self.clear_errors_callback)
+
+        """Set up the statusbar widget for login info"""
+        self.statusbar_widget = HMIApp.StatusbarWidget()
+        self.statusbar.addPermanentWidget(self.statusbar_widget, stretch=1)
+
+        """Set up login level"""
+        # command line arguments
+        if sys.argv is not None:
+            print(sys.argv)
+            if self.__NO_LOGIN in sys.argv:
+                # no login mode
+                self.user_level = LoginStatus.ADMIN
+
+            else:
+                self.user_level = LoginStatus.LOGGED_OUT
+
+            if self.__NO_LOCK in sys.argv:
+                self.ui_locking = False
+
+            else:
+                self.ui_locking = True
+
+        self.logger.info("Base class constructor done")
+
+    # region OVERRIDES *************************************************************************************************
+
+    def closeEvent(self, event) -> None:
+        self.ros_worker.stop()
+        self.ros_thread.quit()
+        self.ros_thread.wait()
+
+    # endregion
+
+    # region ROS CALLBACKS *********************************************************************************************
+
+    def ros_node_online_callback(self):
+        pass
+
+    def nodes_loaded_callback(self, dependencies: List[str], missing: List[str]):
+        """Callback method for displaying the loaded nodes"""
+
+        self.list_nodes.clear()  # clear the nodes list
+
+        for row, node in enumerate(dependencies):  # iterate through the dependency nodes
+            item_widget = HMIApp.NodeListItem(node, missing=(node in missing))
+            list_widget_item = QListWidgetItem(self.list_nodes)  # add a new list widget item
+            list_widget_item.setSizeHint(item_widget.sizeHint())  # copy the size hint from the item widget
+            self.list_nodes.addItem(list_widget_item)
+            self.list_nodes.setItemWidget(list_widget_item, item_widget)
+
+    def info_loaded_callback(self):
+        """Callback method for consuming the KPI info returned by the ROS thread"""
+        pass
+
+    def ros_error_callback(self, node_name: str, msg: str, err_code: int):
+        item_widget = HMIApp.ErrorListItem(node_name=node_name, error_msg=msg, error_code=err_code)
+        list_widget_item = QListWidgetItem(self.list_error)  # add a new list widget item
+        list_widget_item.setSizeHint(item_widget.sizeHint())  # copy the size hint from the item widget
+        self.list_error.addItem(list_widget_item)   # add a new item
+        self.list_error.setItemWidget(list_widget_item, item_widget)  # set the item widget to the item
+
+        self.statusbar_widget.set_error_count(self.list_error.count())
+
+    # endregion
+
+    # region CALLBACKS *************************************************************************************************
+
+    def tab_changed_callback(self, index: int):
+        """Callback function for when the current selected tab changes"""
+        if index == self.__TAB_INFO:
+            pass
+
+        elif index == self.__TAB_CONTROL:
+            pass
+
+        elif index == self.__TAB_NODES:
+            self.ros_worker.load_nodes()
+
+        elif index == self.__TAB_GRAFANA:
+            pass
+            # self.web_widget.load(QUrl(IPList.Grafana.value))
+            # self.web_widget.show()
+
+        elif index == self.__TAB_ERROR:
+            pass
+
+        elif index == self.__TAB_LOGIN:
+            pass
+
+    def login_callback(self):
+        """This method gets called when the login button gets pressed"""
+        name = self.line_edit_name.text()
+        passwd = self.line_edit_pass.text()
+
+        try:
+            level = LoginStatus(name)  # gets the login level /USER ADMIN MAINTENANCE/ or throws KeyError
+            if passwd == LoginStatus.get_pass(level):  # if the password for the level is matching
+                self.user_level = level
+                self.label_login.setText("")
+
+            else:
+                self.label_login.setText("Invalid password")
+
+        except KeyError:
+            self.label_login.setText("Invalid username")
+
+    def logout_callback(self):
+        """This method gets called when the logout button is pressed"""
+        self.user_level = LoginStatus.LOGGED_OUT
+
+    def clear_errors_callback(self):
+        """Callback method for the 'CLEAR ERRORS' button"""
+        self.list_error.clear()
+        self.statusbar_widget.set_error_count(0)
+
+    # endregion
+
+    # region METHODS ***************************************************************************************************
+
+    def load_nodes(self):
+        self.ros_worker.load_nodes()
+
+    def load_info(self):
+        self.ros_worker.load_info()
+
+    def set_interface_lock(self):
+        """Locks the interface elements according to the user level"""
+        if self.user_level == LoginStatus.LOGGED_OUT:
+            self.tabWidget.setTabEnabled(0, False)  # info locked
+            self.tabWidget.setTabEnabled(1, False)  # control locked
+            self.tabWidget.setTabEnabled(2, True)  # nodes enabled
+            self.tabWidget.setTabEnabled(3, False)  # grafana locked
+            self.tabWidget.setTabEnabled(4, True)  # error log enabled
+            self.tabWidget.setTabEnabled(5, True)  # login tab enabled
+
+        elif self.user_level == LoginStatus.USER:
+            self.tabWidget.setTabEnabled(0, True)  # info enabled
+            self.tabWidget.setTabEnabled(1, True)  # control enabled
+            self.tabWidget.setTabEnabled(2, True)  # nodes enabled
+            self.tabWidget.setTabEnabled(3, False)  # grafana locked
+            self.tabWidget.setTabEnabled(4, True)  # error log enabled
+            self.tabWidget.setTabEnabled(5, True)  # login tab enabled
+
+        elif self.user_level == LoginStatus.MANAGER:
+            self.tabWidget.setTabEnabled(0, True)  # info locked
+            self.tabWidget.setTabEnabled(1, False)  # control locked
+            self.tabWidget.setTabEnabled(2, True)  # nodes enabled
+            self.tabWidget.setTabEnabled(3, True)  # grafana enabled
+            self.tabWidget.setTabEnabled(4, True)  # error log enabled
+            self.tabWidget.setTabEnabled(5, True)  # login tab enabled
+
+        elif self.user_level == LoginStatus.ADMIN:
+            self.tabWidget.setTabEnabled(0, True)  # info enabled
+            self.tabWidget.setTabEnabled(1, True)  # control enabled
+            self.tabWidget.setTabEnabled(2, True)  # nodes enabled
+            self.tabWidget.setTabEnabled(3, True)  # grafana enabled
+            self.tabWidget.setTabEnabled(4, True)  # error log enabled
+            self.tabWidget.setTabEnabled(5, True)  # login tab enabled
+
+    # endregion
+
+    # region PROPERTIES ************************************************************************************************
+
+    @property
+    def user_level(self):
+        return self.__user_level
+
+    @user_level.setter
+    def user_level(self, value: LoginStatus):
+        self.__user_level = value
+        if value == LoginStatus.LOGGED_OUT:
+            self.statusbar_widget.label_login.setText(f"Not signed in")
+
+        else:
+            self.statusbar_widget.label_login.setText(f"Signed in as {value.value}")
+
+        self.set_interface_lock()
+
+    # endregion
+
+
+class ROSWorker(QObject):
+    """Class to implement a Worker object to manage thread-safe interaction between the ROS node and the GUI"""
+
+    mutex = QMutex()
+
+    """ SIGNALS """
+    finished = pyqtSignal()
+
+    """ SIGNALS FOR THE ROS NODE """
+
+    class Signals(QObject):
+        started = pyqtSignal()
+        nodes_loaded = pyqtSignal(list, list)
+        info_loaded = pyqtSignal()
+        new_error = pyqtSignal(str, str, int)
+        dependency_online = pyqtSignal(str, bool)
+        status = pyqtSignal(object)
+
+    def __init__(self,
+                 node_class: Type[QTROSNode],
+                 node_name: str,
+                 depends_list: List[str]
+                 ):
+        super(ROSWorker, self).__init__()
+        self.__node_class = node_class
+        self.__ros_node = None
+        self.__node_name = node_name
+        self.__depends_list = depends_list
+        self.__signals = ROSWorker.Signals()
+
+        self.__ready = False
+
+    # noinspection PyUnresolvedReferences
+    def add_to_thread(self, thread: QThread):
+        """Adds the worker object to the thread"""
+        self.moveToThread(thread)
+        thread.started.connect(self.run)
+        self.finished.connect(thread.quit)
+        self.finished.connect(self.deleteLater)
+        self.finished.connect(thread.deleteLater)
+
+    def run(self):
+        """Task for running the ROS node"""
+        self.__ros_node = self.__node_class(node_name=self.__node_name,
+                                            depends_on=self.__depends_list,
+                                            signals=self.signals
+                                            )
+        rclpy.spin(self.__ros_node)
+
+        self.mutex.lock()
+        self.__ready = True
+        self.mutex.unlock()
+
+    @pyqtSlot()
+    def load_info(self):
+        """Loads the info to the INFO tab of the hmi"""
+        print(self.ready)
+        self.__ros_node.load_info()
+
+    @pyqtSlot()
+    def load_nodes(self):
+        assert isinstance(self.__ros_node, QTROSNode)
+        self.__ros_node.load_nodes()
+
+    @pyqtSlot()
+    def send_control(self, *args, **kwargs):
+        assert isinstance(self.__ros_node, QTROSNode)
+        self.__ros_node.send_control(*args, **kwargs)
+
+    @pyqtSlot()
+    def send_kpi(self, *args, **kwargs):
+        self.__ros_node.send_kpi(*args, **kwargs)
+
+    @pyqtSlot()
+    def stop(self):
+        self.__ros_node.get_logger().info("Shutting down ROS node")
+        rclpy.shutdown()
+        self.finished.emit()
+
+    @pyqtSlot()
+    def missing_dependencies(self):
+        self.mutex.lock()
+        missing = self.__ros_node.missing_dependencies
+        self.mutex.unlock()
+
+        return missing
+
+    @property
+    def signals(self):
+        return self.__signals
+
+    @property
+    def ready(self):
+        """Returns if the ROS node is properly initialized"""
+        return self.__ready
---
+++ mediscara_ws/src/mediscara/mediscara/scripts/ros_node.py
@@ -0,0 +1,189 @@
+from typing import List
+
+from rclpy.node import Node
+
+from interfaces.msg import Error
+from mediscara.scripts.utils import ErrorClass as UtilsError
+
+from abc import ABC, abstractmethod
+
+
+class ROSNode(Node, ABC):
+    """Class for a ROS node for a robot cell"""
+
+    __DEPENDENCY_CHECK_INTERVAL = 1  # s
+
+    def __init__(self, node_name: str, depends_on=None):
+        """Initialization method
+
+        :returns: None
+        :param node_name the name of the node
+        :param depends_on list of nodes on which this node depends
+        :type node_name str
+        :type depends_on list(str)
+        """
+        super(ROSNode, self).__init__(f"{node_name}_node")
+
+        """Create dependency check"""
+        self.__dependencies = depends_on
+        self.__missing_depends_prev_state = None
+        self.__missing_depends_prev = None
+        self.__depends_timer = self.create_timer(ROSNode.__DEPENDENCY_CHECK_INTERVAL, self.__depends_callback)
+
+        if not self.dependencies_ok:
+            missing = self.missing_dependencies
+            self.get_logger().warn(f"Missing dependency nodes: \n{missing}")
+            self.__missing_depends_prev_state = True
+            self.__missing_depends_prev = missing
+
+        else:
+            self.__missing_depends_prev_state = False
+            self.__missing_depends_prev = []
+
+        """Create error and status subscription on dependencies"""
+        if self.__dependencies is not None:
+            for node in self.__dependencies:
+                # error
+                self.create_subscription(
+                    msg_type=Error,
+                    topic=f"{node}_error",
+                    callback=self.error_callback,
+                    qos_profile=10
+                )
+
+                self.get_logger().debug(f'Error subscription for node "{node}" created')
+
+        """Create error publisher"""
+        self.__error_publisher = self.create_publisher(
+            msg_type=Error,
+            topic=f'{node_name}_error',
+            qos_profile=10
+        )
+
+    """ CALLBACKS ---------------------------------------------------------------------------------------------------"""
+
+    def __depends_callback(self):
+        """Checks is the missing dependencies have come online"""
+        # check if a single dependency has come online
+        if self.missing_dependencies != self.__missing_depends_prev:
+            if len(self.__missing_depends_prev) > len(self.missing_dependencies):
+                # a dependency has come online
+                new_nodes_online = set(self.__missing_depends_prev).symmetric_difference(set(self.missing_dependencies))
+
+                for new in new_nodes_online:
+                    self.dependency_online(new, True)
+
+            if len(self.__missing_depends_prev) < len(self.missing_dependencies):
+                new_nodes_offline = set(self.missing_dependencies).symmetric_difference(
+                    set(self.__missing_depends_prev))
+
+                for new in new_nodes_offline:
+                    self.dependency_online(new, False)
+
+            self.__missing_depends_prev = self.missing_dependencies  # update the previous state
+
+        # check if all dependencies have come online
+        if self.dependencies_ok and self.__missing_depends_prev_state:
+            self.__missing_depends_prev_state = False
+            self.all_depends_online()
+
+        elif not self.dependencies_ok and not self.__missing_depends_prev_state:
+            self.depends_offline()
+            self.__missing_depends_prev_state = True
+
+    def error_callback(self, msg: Error):
+        """Callback method for the error messages
+        The child class must override this method
+        """
+        raise NotImplementedError("A subclass must implement this method if it has dependency nodes")
+
+    """ METHODS -----------------------------------------------------------------------------------------------------"""
+
+    def dependency_online(self, name: str, online: bool):
+        """Method to notify subclass that a dependency has come online"""
+        raise NotImplementedError("A subclass must implement this method if it has dependency nodes")
+
+    def all_depends_online(self):
+        """Method to notify subclass that all the dependencies have come online
+        The child class must override this method
+        """
+        raise NotImplementedError("A subclass must implement this method if it has dependency nodes")
+
+    def depends_offline(self):
+        """Method to notify subclass that the dependencies have gone offline
+        The child class must override this method
+        """
+        raise NotImplementedError("A subclass must implement this method if it has dependency nodes")
+
+    def publish_error(self, e: UtilsError):
+        """Method for publishing error messages to the generated topic"""
+        error = Error()
+        error.node_name = self.get_name()
+        error.error_msg = e.error_msg
+        error.error_code = e.error_code
+
+        self.__error_publisher.publish(error)
+
+    """ PROPERTIES --------------------------------------------------------------------------------------------------"""
+
+    @property
+    def missing_dependencies(self):
+        """Returns the missing nodes
+
+        :returns: a list of all the nodes that are not online
+        """
+        if not self.__dependencies:
+            return []
+
+        missing_depends = list()
+        node_names = self.get_node_names()
+        for node in self.__dependencies:
+            if f"{node}_node" not in node_names:
+                missing_depends.append(node)
+
+        return missing_depends
+
+    @property
+    def dependencies_ok(self):
+        """Returns True if all the dependencies are online, False otherwise"""
+        if self.__dependencies is None:
+            return True
+
+        node_names = self.get_node_names()
+        return all(f'{node_name}_node' in node_names for node_name in self.__dependencies)
+
+    @property
+    def dependencies(self):
+        """Returns the dependencies of the node"""
+        return self.__dependencies
+
+
+class QTROSNode(ROSNode, ABC):
+    """ROS Node to use in QT applications"""
+
+    def __init__(self, node_name: str, depends_on: List[str], signals):
+        super(QTROSNode, self).__init__(node_name=node_name, depends_on=depends_on)
+        self.__signals = signals
+
+    @abstractmethod
+    def load_info(self):
+        """Load info about the KPI"""
+        pass
+
+    @abstractmethod
+    def load_nodes(self):
+        """Load info about the online nodes"""
+        pass
+
+    @abstractmethod
+    def send_control(self, *args, **kwargs):
+        """Send messages to the respective control topics"""
+        pass
+
+    @abstractmethod
+    def send_kpi(self, *args, **kwargs):
+        pass
+
+    @property
+    def signals(self):
+        return self.__signals
