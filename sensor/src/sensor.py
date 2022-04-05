fde8aeb3f77c90cb920546ab1268a2ccd3866254
Puska Péter <ppuska@lasram.hu>
2022-04-05 14:36:47 +0200

    updated data model

--- sensor/src/sensor.py
+++ sensor/src/sensor.py
@@ -1,68 +1,27 @@
 """Module for the Sensor Node in the cell"""
-import dataclasses
 import json
-import random
+from random import random
 import sys
 import time
-from typing import Any, ClassVar, List
 import logging
 from dataclasses import dataclass, field
-import math
 import re
 
 import requests
 import simplejson
 
-@dataclass
-class JSONField:
-    """Class for storing a single JSON Field"""
-    type: str
-    value: Any
+from ngsi import DataModel, JSONContent
+
 
 @dataclass
-class Sensor:
+class SensorData(DataModel):
     """Class for managing sensor data in the cell"""
 
-    id: str = "sensor_data"
-    type: str = "Sensor"
+    content: JSONContent = field(default=JSONContent("temperature", "humidity"))
 
-    temperature: JSONField = field(default=JSONField(type="Number", value=0.0))
-    humidity: JSONField = field(default=JSONField(type="Number", value=0.0))
-
-    c: ClassVar[float] = random.random()
-
-    @classmethod
-    def field_names(cls, with_header: bool = True) -> List[str]:
-        """Returns the field names of this class"""
-        if with_header:
-            return [field_.name for field_ in dataclasses.fields(cls)]
-
-        result = list()
-        for field_ in dataclasses.fields(cls):
-            if field_.name != "id" or field_.name != "type":
-                result.append(field_.name)
-
-        return result
-
-    def to_json(self, with_header: bool = True) -> str:
-        """Returns the class as a JSON formatted string"""
-        data = dataclasses.asdict(self)
-        if with_header:
-            return json.dumps(data)
-
-        else:
-            del data["id"]
-            del data["type"]
-
-            return json.dumps(data)
-
-
-    def get_data(self):
-        """Fetches the data from the sensor"""
-        self.temperature.value = (math.sin(self.c) + 1) * 50
-        self.humidity.value = (math.cos(self.c) + 1) * 100
-
-        self.c += 0.1
+    def update(self):
+        self.content.temperature.value = 23 + random() * 5
+        self.content.humidity.value = 65 + random() * 10
 
 class FiwareCommunicator:
     """Class for communicating with the FIWARE Orion Context Broker"""
@@ -70,20 +29,19 @@ class FiwareCommunicator:
     CB_URL = "http://localhost:1026"
 
     def __init__(self) -> None:
-        logging.basicConfig(level=logging.DEBUG)
+        logging.basicConfig(level=logging.INFO)
 
-        self.sensor = Sensor()
-        self.sensor.get_data()
+        self.sensor = SensorData()
+        self.sensor.update()
 
         # check if entity exists
-        result = requests.get(url=f"{self.CB_URL}/v2/entities/{self.sensor.id}")
+        result = requests.get(url=f"{self.CB_URL}/v2/entities/{self.sensor.header.id}")
 
         if result.status_code == 404: # entity not found
             sensor_json = self.sensor.to_json()
             logging.info("Entity not found, creating new, payload: %s", sensor_json)
             result = requests.post(url=f"{self.CB_URL}/v2/entities",
-                                   data=sensor_json,
-                                   headers={"content-Type": "application/json"}
+                                   json=sensor_json
                                    )
             if result.status_code == 201:  # created == success
                 logging.info("Operation successful")
@@ -115,7 +73,7 @@ class FiwareCommunicator:
                 for entity in entities:
                     id_pattern = entity["idPattern"]
 
-                    result = re.search(string=self.sensor.id, pattern=id_pattern)
+                    result = re.search(string=self.sensor.header.id, pattern=id_pattern)
 
                     if result is not None:
                         return
@@ -137,10 +95,10 @@ class FiwareCommunicator:
             "description": "Notify QuantumLeap that sensor values are updated",
             "subject": {
                 "entities": [{
-                    "idPattern": f"{self.sensor.id}"
+                    "idPattern": f"{self.sensor.header.id}"
                 }],
                 "condition": {
-                    "attrs": self.sensor.field_names()
+                    "attrs": self.sensor.field_names(with_header=False)
                 }
             },
             "notification": {
@@ -150,8 +108,6 @@ class FiwareCommunicator:
                 "attrs": self.sensor.field_names(with_header=False)
             }
         }
-
-        print(data)
 
         response = requests.post(url=f"{self.CB_URL}/v2/subscriptions",
                                  headers={"Content-Type": "application/json"},
@@ -165,11 +121,10 @@ class FiwareCommunicator:
 
     def update(self):
         """Updates the sensor value in the OCB"""
-        self.sensor.get_data()  # fetch sensor data
+        self.sensor.update()  # fetch sensor data
 
-        response = requests.patch(url=f"{self.CB_URL}/v2/entities/{self.sensor.id}/attrs",
-                                  headers={"Content-Type": "application/json"},
-                                  data=self.sensor.to_json(with_header=False)
+        response = requests.patch(url=f"{self.CB_URL}/v2/entities/{self.sensor.header.id}/attrs",
+                                  json=self.sensor.to_json(with_header=False)
                                   )
 
         if response.status_code == 204:  # no content == success
@@ -184,6 +139,10 @@ class FiwareCommunicator:
             time.sleep(5)
 
             self.update()
+            print(f"Current temperature: {self.sensor.content.temperature.value}" \
+                  f"°C, Current humidity: {self.sensor.content.humidity.value} %H",
+                  end="\r"
+                  )
 
 
 
