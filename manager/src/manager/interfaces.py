868a6390466a484cd4198bdba16e25487ecf919d
Puska Péter <ppuska@lasram.hu>
2022-05-18 13:11:45 +0200

    added CRUD operations

---
+++ manager/src/manager/interfaces.py
@@ -0,0 +1,117 @@
+"""Module for storing interface classes"""
+
+import logging
+from enum import Enum
+
+import requests
+
+
+class StatusCodes(Enum):
+    """Enum class to store the HTTP request response status codes"""
+
+    OK = 200
+    CREATED = 201
+    NO_CONTENT = 204
+    NOT_FOUND = 404
+    UNPROCESSABLE = 422
+
+
+class FIWARE:
+    """Communication class for interfacing with the Orion Context Broker"""
+
+    OCB_PORT = 1026
+
+    def __init__(self, server_addr: str) -> bool:
+        """Initializes the communication to the FIWARE OCB
+
+        Args:
+            server_addr (str): The IPv4 address of the OCB server
+
+        Raises:
+            ConnectionError: If the connection cannot be established
+        """
+        self.__server_address = f"{server_addr}:{self.OCB_PORT}"
+
+        # test the connection to the server
+        try:
+            response = requests.get(f"http://{self.__server_address}/v2")
+
+            if response.status_code == StatusCodes.OK.value:
+                logging.info("Communication initialized, server online")
+
+            else:
+                logging.info("Communication initalized, server unreachable (response %i)", response.status_code)
+
+        except requests.exceptions.ConnectionError as error:
+            logging.error("Failed to establish connection")
+            raise ConnectionError from error
+
+    def create_entity(self, entity: dict) -> bool:
+        """Creates an entity in the OCB
+
+        Args:
+            entity (dict): The JSON representation formatted as NGSIv2
+
+        Returns:
+            bool: Whether the operation was successful or not
+        """
+
+        response = requests.post(f"http://{self.__server_address}/v2/entities", json=entity)
+
+        if response.status_code == StatusCodes.CREATED.value:
+            return True
+
+        logging.warning("Could not create new entity (response: %s)", response.content)
+        return False
+
+    def get_entity(self, entity_id: str) -> dict or None:
+        """Return the entity with the given id from the OCB
+
+        Args:
+            entity_id (str): The id of the entity
+
+        Returns:
+            dict or None: The JSON representation of the entity, or None if the entity was not found
+        """
+
+        response = requests.get(f"http://{self.__server_address}/v2/entities/{entity_id}")
+
+        if response.status_code == StatusCodes.NOT_FOUND.value:
+            return None
+
+        return response.json()
+
+    def update_entity(self, entity_id: str, attrs: dict) -> bool:
+        """Updates the entity with the given id in the OCB
+
+        Args:
+            entity_id (str): The ID of the entity
+            attrs (dict): the attributes of the entity to be updated
+
+        Returns:
+            bool: Whether the operation was successful
+        """
+        response = requests.patch(f"http://{self.__server_address}/v2/entities/{entity_id}/attrs", json=attrs)
+
+        if response.status_code == StatusCodes.NO_CONTENT.value:
+            return True
+
+        logging.warning("Unable to update entity '%s'. (response %s)", entity_id, response.content)
+        return False
+
+    def delete_entity(self, entity_id: str):
+        """Deletes the entity from the OCB
+
+        Args:
+            entity_id (str): The ID of the entity
+
+        Returns:
+            _type_: Whether the operation was successful or not
+        """
+        response = requests.delete(f"http://{self.__server_address}/v2/entities/{entity_id}")
+
+        if response.status_code == StatusCodes.NO_CONTENT.value:
+            return True
+
+        logging.warning("Unable to delete entity '%s'. (response %s'", entity_id, response.content)
+        return False
---
+++ manager/tests/test_interfaces.py
@@ -0,0 +1,31 @@
+"""Testing module for the interfaces module"""
+
+import logging
+
+from manager.interfaces import FIWARE
+
+
+def test_connection():
+    """This method tests the init method of the fiware interface"""
+
+    entity_id = "manager.message"
+    entity_attrs = {"content": {"type": "Text", "value": "test message"}}
+    entity_attrs_updated = {"content": {"type": "Text", "value": "test message2"}}
+
+    entity = {"id": entity_id, "type": "Message", "content": entity_attrs.get("content")}
+
+    try:
+        f_connector = FIWARE("25.18.161.28")
+
+    except ConnectionError:
+        logging.fatal("Connection error")
+
+    f_connector.delete_entity(entity_id=entity_id)
+
+    assert f_connector.create_entity(entity=entity) is True
+    assert f_connector.update_entity(entity_id=entity_id, attrs=entity_attrs_updated) is True
+    assert f_connector.delete_entity(entity_id=entity_id) is True
+
+
+if __name__ == "__main__":
+    test_connection()
